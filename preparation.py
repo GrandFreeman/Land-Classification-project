7df729b380d36e3586c976bdaedd7c9b0a99aa1c
GrandFreeman <54396083+GrandFreeman@users.noreply.github.com>
2026-02-01 10:50:30 +0800

    Update preparation.py

--- preparation.py
+++ preparation.py
@@ -3,15 +3,25 @@ dataset_url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.clou
 
 import os
 import skillsnetwork
+import asyncio
 
 from def_frames import check_skillnetwork_extraction
 from def_frames import download_tar_dataset
 from def_frames import download_model
 
 
+async def main_skill():
+    await skillsnetwork.prepare(url=dataset_url, path=data_dir, overwrite=True)
+async def main_tar():
+    await download_tar_dataset(dataset_url, tar_path, data_dir)
+async def main():
+    await download_model(pytorch_state_dict_url, pytorch_state_dict_path)
+
+
 try:
     check_skillnetwork_extraction(data_dir)
-    await skillsnetwork.prepare(url=dataset_url, path=data_dir, overwrite=True)
+    if __name__ == "__main_skill__":
+        asyncio.run(main_skill())
 except Exception as e:
     print(e)
     print("Primary download/extraction method failed.")
@@ -21,12 +31,14 @@ except Exception as e:
     from pathlib import Path
     file_name = Path(dataset_url).name
     tar_path = os.path.join(data_dir, file_name)
-    await download_tar_dataset(dataset_url, tar_path, data_dir)
+    if __name__ == "__main_tar__":
+        asyncio.run(main_tar())
 
 
 pytorch_state_dict_url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/rFBrDlu1NNcAzir5Uww8eg/pytorch-cnn-vit-ai-capstone-model-state-dict.pth"
 pytorch_state_dict_name = "pytorch_cnn_vit_ai_capstone_model_state_dict.pth"
 pytorch_state_dict_path = os.path.join(data_dir, pytorch_state_dict_name)
 
-await download_model(pytorch_state_dict_url, pytorch_state_dict_path)
+if __name__ == "__main__":
+    asyncio.run(main())
 
