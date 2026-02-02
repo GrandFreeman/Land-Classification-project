8462d26c92ebe650dc9791a3b6a20f703380df52
GrandFreeman <54396083+GrandFreeman@users.noreply.github.com>
2026-02-02 10:49:33 +0800

    Update def_frame.py

--- def_frame.py
+++ def_frame.py
@@ -1,3 +1,22 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+import httpx
+import random
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+#import torch.optim as optim
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from torch.utils.data import  random_split
+import torch.nn.functional as F
+
 #====================== preparation defs =========================
 
 def check_skillnetwork_extraction(extract_dir):
